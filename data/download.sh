6fa704e1b8fd00aa7fcb41b0390a09822ba00e30
Martino Mensio <martinomensio@outlook.it>
2017-09-26 23:37:00 +0200

    datasets preprocessing the same format for the two datasets

--- data/download.sh
+++ data/download.sh
@@ -4,6 +4,6 @@ WIT_TOKEN=`python get_link.py`
 WIT_URL="https://api.wit.ai/export/$WIT_TOKEN"
 
 wget -O wit_data.zip $WIT_URL
-rm -rf BotCycle
-unzip -o wit_data.zip
+rm -rf wit
+unzip -o wit_data.zip -d wit
 rm -rf wit_data.zip
